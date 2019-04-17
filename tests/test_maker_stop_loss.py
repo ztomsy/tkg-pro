20fd4a642e9f04d6c2160dea04480122486d3441
Ivan Averin <i.averin@gmail.com>
2019-04-17 14:52:51 +0300

    added maximum updates in "maker" state

--- tests/test_maker_stop_loss.py
+++ tests/test_maker_stop_loss.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from .context import tkgpro
 from tkgpro import MakerStopLossOrder
-from tkgcore import core
+from tkgcore import core, ccxtExchangeWrapper, ActionOrderManager
+
 import unittest
 
 
@@ -17,6 +18,7 @@ class MakerStopLossOrderTestSuite(unittest.TestCase):
             cancel_threshold=0.001,
             maker_price_threshold=-0.003,
             maker_order_max_updates=60,
+            force_taker_updates=1000,
             taker_price_threshold=-0.02,
             taker_order_max_updates=20,
             threshold_check_after_updates=6
@@ -33,11 +35,12 @@ class MakerStopLossOrderTestSuite(unittest.TestCase):
         self.assertEqual(o.taker_order_max_updates, 20)
         self.assertEqual(o.threshold_check_after_updates, 6)
 
+        self.assertEqual(1000, o.force_taker_updates)
+
         self.assertEqual("maker", o.state)
         self.assertEqual("open", o.status)
 
         self.assertEqual("new", o.order_command)
-
 
     def test_create_from_start_amount(self):
 
@@ -50,6 +53,7 @@ class MakerStopLossOrderTestSuite(unittest.TestCase):
             cancel_threshold=0.001,
             maker_price_threshold=-0.003,
             maker_order_max_updates=60,
+            force_taker_updates=1000,
             taker_price_threshold=-0.02,
             taker_order_max_updates=20,
             threshold_check_after_updates=6
@@ -65,6 +69,8 @@ class MakerStopLossOrderTestSuite(unittest.TestCase):
         self.assertEqual(o.taker_price_threshold, -0.02)
         self.assertEqual(o.taker_order_max_updates, 20)
         self.assertEqual(o.threshold_check_after_updates, 6)
+
+        self.assertEqual(1000, o.force_taker_updates)
 
         self.assertEqual("maker", o.state)
         self.assertEqual("open", o.status)
@@ -121,11 +127,166 @@ class MakerStopLossOrderTestSuite(unittest.TestCase):
         relative_price_diff = core.relative_target_price_difference(side, order_price, current_maker_price)
         self.assertAlmostEqual(relative_price_diff, -0.01, 8)
 
-    def test_single_maker_fill_is_good(self):
-        pass
-
     def test_multi_maker_fill_is_good(self):
-        pass
+        ex = ccxtExchangeWrapper.load_from_id("binancae") # type: ccxtExchangeWrapper
+        ex.set_offline_mode("test_data/markets.json", "test_data/tickers_maker.csv")
+
+        om = ActionOrderManager(ex)
+        om.offline_order_updates = 10  # number of updates to fill order from offline data
+
+        order = MakerStopLossOrder.create_from_start_amount(
+            symbol="BTC/USDT",
+            start_currency="BTC",
+            start_amount=1,
+            dest_currency="USDT",
+            target_amount=1,
+            cancel_threshold=0.001,
+            maker_price_threshold=-0.01,
+            maker_order_max_updates=60,
+            taker_price_threshold=-0.02,
+            taker_order_max_updates=20,
+            threshold_check_after_updates=6
+        )
+        om.add_order(order)
+        om.proceed_orders()  # update 1: order creation
+
+        market_data = {"tickers": {"BTC/USDT": {"ask": 1, "bid": 0.99}}}
+
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()  # update 1: order fill 1/10 from 1 (0.1)
+
+        self.assertEqual("maker", order.state)
+        self.assertEqual("hold tickers BTC/USDT", order.order_command)
+
+        # maker price (ask) below threshold
+
+        market_data = {"tickers": {"BTC/USDT": {"ask": 0.6, "bid": 0.99}}}
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()  # update 2: order fill 2/10 from 1 (0.)
+
+        self.assertEqual(order.state, "maker")
+        self.assertEqual("cancel tickers BTC/USDT", order.order_command)
+        self.assertIn("#below_threshold_maker", order.tags)
+        self.assertEqual(0.2, order.filled)
+
+        # first trade order canceled. new trade order request for maker price 0.999
+        market_data = {"tickers": {"BTC/USDT": {"ask": 0.999, "bid": 0.99}}}
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()
+
+        self.assertEqual(0.999, order.price)
+        self.assertEqual(0.2, order.orders_history[-1].filled)
+        self.assertEqual(order.state, "maker")
+        self.assertEqual("new tickers BTC/USDT", order.order_command)
+        self.assertEqual(0.2, order.filled)
+
+        # new order created
+        market_data = {"tickers": {"BTC/USDT": {"ask": 0.999, "bid": 0.99}}}
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()
+
+        self.assertEqual(0.2, order.filled)
+        self.assertEqual("open", order.active_trade_order.status)
+        self.assertEqual(0.8, order.active_trade_order.amount)
+
+        # 1st update of the new trade order. filled 1/10 of the new order (0.08 from 0.8)
+        market_data = {"tickers": {"BTC/USDT": {"ask": 0.999, "bid": 0.99}}}
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()
+
+        self.assertEqual(0.28, order.filled)
+
+    def test_multi_maker_fill_from_ticker(self):
+
+        ex = ccxtExchangeWrapper.load_from_id("binancae") # type: ccxtExchangeWrapper
+        ex.set_offline_mode("test_data/markets.json", "test_data/tickers_maker.csv")
+
+        om = ActionOrderManager(ex)
+        om.offline_order_updates = 10  # number of updates to fill order from offline data
+
+        order = MakerStopLossOrder.create_from_start_amount(
+            symbol="BTC/USDT",
+            start_currency="BTC",
+            start_amount=1,
+            dest_currency="USDT",
+            target_amount=1,
+            cancel_threshold=0.001,
+            maker_price_threshold=-0.01,
+            maker_order_max_updates=4,
+            taker_price_threshold=-0.02,
+            taker_order_max_updates=5,
+            threshold_check_after_updates=6
+        )
+
+        om.add_order(order)
+
+        while om.have_open_orders():
+            market_data = {"tickers": {"BTC/USDT": {"ask": 1, "bid": 0.99}}}
+            om.data_for_orders.update(market_data)
+            om.proceed_orders()  # update 1: order creation
+
+        self.assertEqual("closed", order.status)
+        self.assertEqual(1, order.filled_start_amount)
+        self.assertEqual("maker", order.state)
+        self.assertNotIn("#below_threshold_maker", order.tags)
+
+        # number of trade orders created:
+        # each order fills 4/10 of whole amount
+        # ... should be 20 ;)
+        self.assertEqual(20, len(order.orders_history))
+
+    def test_maker_force_taker_updates(self):
+        ex = ccxtExchangeWrapper.load_from_id("binancae") # type: ccxtExchangeWrapper
+        ex.set_offline_mode("test_data/markets.json", "test_data/tickers_maker.csv")
+
+        om = ActionOrderManager(ex)
+        om.offline_order_updates = 10  # number of updates to fill order from offline data
+
+        order = MakerStopLossOrder.create_from_start_amount(
+            symbol="BTC/USDT",
+            start_currency="BTC",
+            start_amount=1,
+            dest_currency="USDT",
+            target_amount=1,
+            cancel_threshold=0.001,
+            maker_price_threshold=-0.01,
+            maker_order_max_updates=4,
+            force_taker_updates=2,
+            taker_price_threshold=-0.02,
+            taker_order_max_updates=5,
+            threshold_check_after_updates=6
+        )
+
+        om.add_order(order)
+
+        market_data = {"tickers": {"BTC/USDT": {"ask": 1, "bid": 0.99}}}
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()
+
+        market_data = {"tickers": {"BTC/USDT": {"ask": 1, "bid": 0.99}}}
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()
+
+        self.assertIn("#force_taker_max_maker_updates", order.tags)
+        self.assertEqual("taker", order.state)
+        self.assertEqual("cancel tickers BTC/USDT", order.order_command)
+
+        market_data = {"tickers": {"BTC/USDT": {"ask": 1, "bid": 0.99}}}
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()
+
+        self.assertEqual("new tickers BTC/USDT", order.order_command)
+        self.assertEqual(0.99, order.active_trade_order.price)
+        self.assertEqual("taker", order.state)
+
+        market_data = {"tickers": {"BTC/USDT": {"ask": 1, "bid": 0.99}}}
+        om.data_for_orders.update(market_data)
+        om.proceed_orders()
+
+        self.assertEqual(0.99, order.active_trade_order.price)
+        self.assertEqual("taker", order.state)
+        self.assertEqual(1, len(order.orders_history))
+
 
     def test_maker_threshold_triggered(self):
         pass
@@ -134,6 +295,12 @@ class MakerStopLossOrderTestSuite(unittest.TestCase):
         pass
 
     def test_multi_taker_fill_is_good(self):
+        pass
+
+    def test_force_taker(self):
+        """
+        if total updates in maker state exceed force_taker_updates
+        """
         pass
 
 
--- tkgpro/threshold_order/maker_stop_loss.py
+++ tkgpro/threshold_order/maker_stop_loss.py
@@ -15,6 +15,7 @@ class MakerStopLossOrder(ActionOrder):
                  cancel_threshold: float = 0.000001,
                  maker_price_threshold: float = 0.005,
                  maker_order_max_updates: int = 50,
+                 force_taker_updates: int = 500,
                  taker_price_threshold: float = -0.01,
                  taker_order_max_updates: int = 10,
                  threshold_check_after_updates: int = 5):
@@ -28,6 +29,7 @@ class MakerStopLossOrder(ActionOrder):
         this threshold. This is for avoiding the situation of creating new order for less than minimun amount. Usually
         should be minimum order amount/value for the order's pair + commission.
              In ccxt: markets[symbol]["limits"]["amount"]["min"]
+        :param force_taker_updates: total number of updates to force the taker state
         :param max_order_updates: order updates before cancelling
         :param taker_price_threshold:  relative difference between the order's price and current taker price. Should be
         negative for changing price in a "bad" way
@@ -38,9 +40,15 @@ class MakerStopLossOrder(ActionOrder):
         self.maker_price_threshold = maker_price_threshold
         self.maker_order_max_updates = maker_order_max_updates
 
+        self.force_taker_updates = force_taker_updates
         self.taker_price_threshold = taker_price_threshold
         self.taker_order_max_updates = taker_order_max_updates
         self.threshold_check_after_updates = threshold_check_after_updates
+
+        self._total_maker_updates = 0
+        """
+        total updates in marker state
+        """
 
         super().__init__(symbol, amount, price, side, cancel_threshold, taker_order_max_updates)
 
@@ -49,6 +57,7 @@ class MakerStopLossOrder(ActionOrder):
                                  cancel_threshold: float = 0.000001,
                                  maker_price_threshold: float = 0.005,
                                  maker_order_max_updates: int = 50,
+                                 force_taker_updates: int = 500,
                                  taker_price_threshold: float = -0.01,
                                  taker_order_max_updates: int = 10,
                                  threshold_check_after_updates: int = 5):
@@ -66,6 +75,7 @@ class MakerStopLossOrder(ActionOrder):
         :param maker_order_max_updates:, :param taker_order_max_updates: max order updates for each separate order in maker/taker phase
         :param maker_price_threshold: ,  :param taker_price_threshold:  relative difference between the order's price and current taker price. Should be
         negative for changing price in a "bad" way
+        :param force_taker_updates: total number of updates to force the taker state
         :param threshold_check_after_updates: number of order's updates to start requesting ticker and check price
         threshold
         """
@@ -75,7 +85,7 @@ class MakerStopLossOrder(ActionOrder):
         price = core.ticker_price_for_dest_amount(side, start_amount, target_amount)
 
         order = super().create_from_start_amount(symbol, start_currency, start_amount, dest_currency, price,
-                                                 cancel_threshold)
+                                                 cancel_threshold)  # type: MakerStopLossOrder
 
         order.maker_price_threshold = maker_price_threshold
         order.maker_order_max_updates = maker_order_max_updates
@@ -83,6 +93,8 @@ class MakerStopLossOrder(ActionOrder):
         order.taker_price_threshold = taker_price_threshold
         order.taker_order_max_updates = taker_order_max_updates
         order.threshold_check_after_updates = threshold_check_after_updates
+
+        order.force_taker_updates = force_taker_updates
 
         return order
 
@@ -96,71 +108,94 @@ class MakerStopLossOrder(ActionOrder):
 
     def _on_open_order(self, active_trade_order: TradeOrder, market_data=None):
 
-        order_command = "hold"
+        order_command = "hold tickers {symbol}".format(symbol=self.symbol)
+        current_taker_price, current_maker_price = 0, 0
+        # for "maker" and "taker" states
+        # than if market data is present - let's check if it's below or above the thresholds
+        # current_taker_price, current_taker_price = 0, 0
+
+        # if there is no market data - just return default command
+        if market_data is not None:
+            try:
+                ticker_info = core.get_symbol_order_price_from_tickers(self.start_currency,
+                                                                       self.dest_currency,
+                                                                       {self.symbol: market_data[0]})
+                current_taker_price = ticker_info["price"]
+                current_maker_price = ticker_info["maker_price"]
+
+            except Exception as e:
+                order_command = "hold tickers {symbol}".format(symbol=self.symbol)
+                return order_command
+
+        # check taker price for both states
+        if current_taker_price > 0:
+            relative_price_diff = core.relative_target_price_difference(self.side, self.price,
+                                                                        current_taker_price)
+
+            if relative_price_diff is not None and relative_price_diff <= self.taker_price_threshold:
+                order_command = "cancel tickers {symbol}".format(symbol=self.symbol)
+                self.status = "taker"
+
+                if "#below_threshold_taker" not in self.tags:
+                    self.tags.append("#below_threshold_taker_price")
+                return order_command
 
         # maker state
         if self.state == "maker":
 
+            self._total_maker_updates += 1
+
             # let's start requesting the tickers
             order_command = "hold tickers {symbol}".format(symbol=self.symbol)
 
-            # cancel if reach maker_order_max_updates
+            # cancel if reach force_taker_updates
+            if self._total_maker_updates >= self.force_taker_updates \
+                    and self.amount - self.filled > self.cancel_threshold:
+
+                self.tags.append("#force_taker_max_maker_updates")
+                self.state = "taker"
+                return "cancel tickers {symbol}".format(symbol=self.symbol)
+
+            # check if need to re-open maker order becaus of reaching single  maker order updates limit
             if active_trade_order.update_requests_count >= self.maker_order_max_updates \
                     and active_trade_order.amount - active_trade_order.filled > self.cancel_threshold:
-                return "cancel"
-
-            # if there is no market data - just return default command
-            if market_data is None:
-                return order_command
+                return "cancel tickers {symbol}".format(symbol=self.symbol)
 
             order_command = "hold tickers {symbol}".format(symbol=self.symbol)
 
-            # than if market data is present - let's check if it's below or above the thresholds
-            try:
-                ticker_info = core.get_symbol_order_price_from_tickers(self.start_currency,
-                                                                                  self.dest_currency,
-                                                                                  {self.symbol: market_data[0]})
-                current_taker_price = ticker_info["price"]
-                current_maker_price = ticker_info["maker_price"]
+            # check maker price
+            if current_maker_price > 0:
+                relative_price_diff = core.relative_target_price_difference(self.side, self.price,
+                                                                            current_maker_price)
 
-                # check taker price
-                if current_taker_price > 0:
-                    relative_price_diff = core.relative_target_price_difference(self.side, self.price,
-                                                                                current_taker_price)
+                if relative_price_diff is not None and relative_price_diff <= self.maker_price_threshold:
+                    order_command = "cancel tickers {symbol}".format(symbol=self.active_trade_order.symbol)
+                    self.status = "maker"
 
-                    if relative_price_diff is not None and relative_price_diff <= self.taker_price_threshold:
-                        order_command = "cancel"
-                        self.status = "taker"
+                    if "#below_threshold_maker" not in self.tags:
+                        self.tags.append("#below_threshold_maker")
+                    return order_command
 
-                        if "#below_threshold_taker" not in self.tags:
-                            self.tags.append("#below_threshold_taker_price")
-                        return order_command
+        if self.state == "taker":
 
-                # check maker price
-                if current_maker_price > 0:
-                    relative_price_diff = core.relative_target_price_difference(self.side, self.price, current_maker_price)
+            # reset the taker order
+            if active_trade_order.update_requests_count >= self.taker_order_max_updates \
+                    and active_trade_order.amount - active_trade_order.filled > self.cancel_threshold:
 
-                    if relative_price_diff is not None and relative_price_diff <= self.taker_maker_threshold:
-                        order_command = "cancel"
-                        self.status = "maker"
-
-                        if "#below_threshold_maker" not in self.tags:
-                            self.tags.append("#below_threshold_maker")
-                        return order_command
-
-            except Exception as e:
-                order_command = "hold tickers {symbol}".format(symbol=self.symbol)
+                self.stare = "taker"
+                return "cancel tickers {symbol}".format(symbol=self.symbol)
 
         return order_command
 
     def _on_closed_order(self, active_trade_order: TradeOrder, market_data=None):
 
-        self.order_command = "hold tickers {symbol}".format(symbol=self.active_trade_order.symbol)
+        self._close_active_order()
+
+        self.order_command = "hold tickers {symbol}".format(symbol=self.symbol)
 
         if self.filled_start_amount >= self.start_amount * 0.99999:  # close order if filled amount is OK
             self.order_command = ""
-            self._close_active_order()
-            self.close_order()
+            self.close_order()  # we just need to close ActionOrder, the trade order was closed above
             return self.order_command
 
         # check if we have new prioe in market data
@@ -175,12 +210,12 @@ class MakerStopLossOrder(ActionOrder):
 
         if self.state == "maker":
             self.active_trade_order = self._create_next_trade_order_for_remained_amount(current_maker_price)
-            self.order_command = "new"
+            self.order_command = "new tickers {symbol}".format(symbol=self.symbol)
             return self.order_command
 
         if self.state == "taker":
             self.active_trade_order = self._create_next_trade_order_for_remained_amount(current_taker_price)
-            self.order_command = "new"
+            self.order_command = "new tickers {symbol}".format(symbol=self.symbol)
             return self.order_command
 
         return self.order_command
